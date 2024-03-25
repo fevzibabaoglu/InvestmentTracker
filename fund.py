fc8219c5ea0ad07fe871f8bd15bab37ae2b44fa6
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2024-03-25 23:22:08 +0300

    Added save/load to/from json methods for Fund

--- fund.py
+++ fund.py
@@ -1,10 +1,14 @@
+import datetime
 import json
+import os
 import requests
 from bs4 import BeautifulSoup
-from typing import Any, Union
+from unidecode import unidecode
+from typing import Union
 
 
 URL = 'https://www.tefas.gov.tr/FonAnaliz.aspx?FonKod='
+JSON_FOLDER_PATH = 'json'
 
 
 class Fund:
@@ -17,8 +21,8 @@ class Fund:
         self.threeMonthsProfitPercentage = 0.0
         self.sixMonthsProfitPercentage = 0.0
         self.oneYearProfitPercentage = 0.0
-
-        self.getFundInfo()
+        self.date = ''
+        self.loaded = False
 
     def __call__(self) -> dict[str, Union[str, float]]:
         return {
@@ -30,13 +34,14 @@ class Fund:
             'threeMonthsProfitPercentage': self.threeMonthsProfitPercentage,
             'sixMonthsProfitPercentage': self.sixMonthsProfitPercentage,
             'oneYearProfitPercentage': self.oneYearProfitPercentage,
+            'date': self.date,
         }
     
     def __str__(self) -> str:
         return json.dumps(self.__call__(), indent=2, sort_keys=False, ensure_ascii=False)
 
-    def getFundInfo(self) -> None:
-        url = f"{URL}{self.code}"
+    def loadFromWeb(self) -> None:
+        url = f"{URL}{self.code.upper()}"   
         response = requests.get(url=url)
 
         if response.status_code != 200:
@@ -51,10 +56,38 @@ class Fund:
         li_topList = {li.contents[0]: li.span.string for li in content_topList.find_all('li')}
         li_priceIndicators = {li.contents[0]: li.span.string for li in content_priceIndicators.find_all('li')}
 
-        self.label = content_label.string
+        self.label = unidecode(content_label.string)
         self.price = float(li_topList['Son Fiyat (TL)'].replace(',', '.'))
         self.oneDayProfitPercentage = float(li_topList['Günlük Getiri (%)'].replace(',', '.').strip('%'))
         self.oneMonthProfitPercentage = float(li_priceIndicators['Son 1 Ay Getirisi'].replace(',', '.').strip('%'))
         self.threeMonthsProfitPercentage = float(li_priceIndicators['Son 3 Ay Getirisi'].replace(',', '.').strip('%'))
         self.sixMonthsProfitPercentage = float(li_priceIndicators['Son 6 Ay Getirisi'].replace(',', '.').strip('%'))
         self.oneYearProfitPercentage = float(li_priceIndicators['Son 1 Yıl Getirisi'].replace(',', '.').strip('%'))
+        self.date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        self.loaded = True
+
+    def loadFromJson(self) -> None:
+        filename = f"{JSON_FOLDER_PATH}/{self.code}.json"
+
+        if not os.path.exists(filename):
+            raise Exception(f"Error while loading '{self.code}' from json: '{filename}' file does not exist.")
+        
+        with open(filename, 'r') as jsonFile:
+            loadedData = json.load(jsonFile)
+
+            for key, value in loadedData.items():
+                setattr(self, key, value)
+
+        self.loaded = True
+
+    def saveToJson(self) -> None:
+        filename = f"{JSON_FOLDER_PATH}/{self.code}.json"
+
+        if not self.loaded:
+            raise Exception(f"Cannot save an unloaded fund to a json.")
+
+        if not os.path.exists(JSON_FOLDER_PATH):
+            os.makedirs(JSON_FOLDER_PATH)
+
+        with open(filename, 'w') as jsonFile:
+            json.dump(self.__call__(), jsonFile)
--- main.py
+++ main.py
@@ -3,14 +3,22 @@ from fund import Fund
 
 def main():
     ykt = Fund('ykt')
-    yas = Fund('yas')
-    yay = Fund('yay')
-    ybe = Fund('ybe')
+    # yas = Fund('yas')
+    # yay = Fund('yay')
+    # ybe = Fund('ybe')
+
+    ykt.loadFromWeb()
+    # yas.loadFromWeb()
+    # yay.loadFromWeb()
+    # ybe.loadFromWeb()
+
+    ykt.saveToJson()
+    # ykt.loadFromJson()
 
     print(ykt)
-    print(yas)
-    print(yay)
-    print(ybe)
+    # print(yas)
+    # print(yay)
+    # print(ybe)
 
         
 if __name__ == "__main__":
